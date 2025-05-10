f1e76b349d261e4da3d1dce9a32a027c4e939ab4
Dmitrii Rashchenko <dimitree54@gmail.com>
2025-05-10 19:39:03 +0200

    Initial set up

--- src/public_package_template/module.py
+++
@@ -1,8 +0,0 @@
-def mock_function_one():
-    """A mock function that returns a string."""
-    return "Hello from mock_function_one"
-
-
-def mock_function_two(x, y):
-    """A mock function that adds two numbers."""
-    return x + y
--- src/public_package_template/nested/module.py
+++
@@ -1,3 +0,0 @@
-def mock_function_three(x, y):
-    """A mock function that dots two numbers."""
-    return x * y
